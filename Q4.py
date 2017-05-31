8f24b0b314cd24ed7d2b8262705ba8ce65858f42
arunavs94 <asingh94@ucla.edu>
2017-05-31 15:44:56 -0700

    Running condition.

--- Q4.py
+++ Q4.py
@@ -7,62 +7,11 @@ import math
 
 def main():
 
-	# Read from original files
+	# .txt file generated from previous part
+	filename = "data_files/total_5_actors_fixed.csv"
 
-	actors_filepath = "C:/Users/Steven/Desktop/EE232E_Project2/data_files/actor_movies.txt"
-	actresses_filepath = "C:/Users/Steven/Desktop/EE232E_Project2/data_files/actress_movies.txt"
-
-
-	with open(actors_filepath) as f:
-	    reader_actors = csv.reader(f, delimiter="\t")
-	    data_actors = list(reader_actors)
-
-	with open(actresses_filepath) as f:
-	    reader_actress = csv.reader(f, delimiter="\t")
-	    data_actresses = list(reader_actress)
-
-	actors = []
-	actresses = []
-
-	# Create list of all actors and actresses -> total_actors
-	for idx, rows in enumerate(data_actors):
-		# progress check
-		print idx
-
-
-		### To test #####
-		if idx == 50000:
-			break
-		#################
-
-
-
-
-		tmp = [item for item in rows if item != '']
-		actors.append(tmp)
-
-
-	for idx, rows in enumerate(data_actresses):
-		# progress check
-		print idx
-
-
-		### To test #####
-		if idx == 50000:
-			break
-		#################
-
-
-
-		tmp = [item for item in rows if item != '']
-		actresses.append(tmp)
-	
-	total_actors = actors + actresses
-
-	print 'Creating dataframe....'
-
-	# Put data into dataframe
-	total_data = pd.DataFrame(total_actors)
+	# read data into dataframe
+	total_data = pd.read_csv(filename, header=None)
 
 	# create list for actors' names
 	actors = total_data[0].tolist()
@@ -82,7 +31,7 @@ def main():
 
 		# put data of each row into a list
 		tmp = data.tolist()
-		tmp = [item for item in tmp if item is not None]
+		tmp = [item for item in tmp if type(item) != float]
 
 		# seperate out the movies and the corresponding actor
 		tmpMovies = tmp[1:len(tmp)]
@@ -91,6 +40,10 @@ def main():
 		# organize data into dictionaries
 		actors_movies[actor] = tmpMovies
 		[movies_actors[movie].append(actor) for movie in tmpMovies]
+
+		# to handle duplicate movies in actor resumes
+		movies_actors[movie]=list(set(movies_actors[movie]))
+
 		actors_index[actor] = index + 1
 	
 	# create list of movies (is unique)
@@ -109,15 +62,17 @@ def main():
 	# generate list of movies with at least 5 actors
 	core_movies = movies_actors_fin.keys()
 
-
 	text_file = open("data_files/node_list4_snacktors.txt", "w")
 
+	wrong_count = 0
+	errors = []
 
 	# Generate movie-subMovie pairs and weights, write to file
 	for idx, movie in enumerate(core_movies): # [M1, M2, ... , MN] for N total movies
 		# progress check
 		print "Progress of Movie-SubMovie pair/weight .txt file: ", (float(idx))/(len(core_movies))
-
+		print core_movies[idx]
+		print idx
 		match = defaultdict(float)
 
 		personal_actors[movie] = len(movies_actors_fin[movie])
@@ -142,7 +97,16 @@ def main():
 			movie_ = str(key_[0])
 			subMovie_ = str(key_[1])
 
-			weight = (match[key_]) / ( (personal_actors[movie_] + personal_actors[subMovie_]) - match[key_] )
+			denom = ( (personal_actors[movie_] + personal_actors[subMovie_]) - match[key_] )
+			
+			# Compile error cases
+			if denom == 0:
+				wrong_count = wrong_count + 1
+				errors.append(movie_)
+				errors.append(subMovie_)
+				continue
+
+			weight = (match[key_]) / denom
 
 			text_file.write("\"" + movie_ + "\"")
 			text_file.write(',')
@@ -151,10 +115,62 @@ def main():
 			text_file.write(str(weight))
 			text_file.write('\n')
 
+	print errors
+	print wrong_count
 
+####### GRAVEYARD ########
 	
+# Read from original files
+
+	# actors_filepath = "C:/Users/Steven/Desktop/EE232E_Project2/data_files/actor_movies.txt"
+	# actresses_filepath = "C:/Users/Steven/Desktop/EE232E_Project2/data_files/actress_movies.txt"
+
+	# with open(actors_filepath) as f:
+	#     reader_actors = csv.reader(f, delimiter="\t")
+	#     data_actors = list(reader_actors)
+
+	# with open(actresses_filepath) as f:
+	#     reader_actress = csv.reader(f, delimiter="\t")
+	#     data_actresses = list(reader_actress)
+
+	# actors = []
+	# actresses = []
+
+	# # Create list of all actors and actresses -> total_actors
+	# for idx, rows in enumerate(data_actors):
+	# 	# progress check
+	# 	print idx
 
 
+	# 	### To test #####
+	# 	if idx == 50000:
+	# 		break
+	# 	#################
+
+	# 	tmp = [item for item in rows if item != '']
+	# 	actors.append(tmp)
+
+
+	# for idx, rows in enumerate(data_actresses):
+	# 	# progress check
+	# 	print idx
+
+	# 	### To test #####
+	# 	# if idx == 50000:
+	# 	# 	break
+	# 	#################
+
+	# 	tmp = [item for item in rows if item != '']
+	# 	actresses.append(tmp)
+	
+	# total_actors = actors + actresses
+
+	# print 'Creating dataframe....'
+
+	# # Put data into dataframe
+	# total_data = pd.DataFrame(total_actors)
+
+#############
 
 
 if __name__ == '__main__':
